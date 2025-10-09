7175b68e35c34eeae3a1b4fc7b6e7b4e626ca13c
dongcx-com <2578054246@qq.com>
2025-10-09 18:17:59 +0800

    chore(packages): :hammer: +scripts to build packages

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,6 @@
 export default {
-	"*.{js,ts,mjs,cjs,json,tsx,css,less,scss,vue,html,md}": ["cspell lint"],
+	"*.{js,ts,mjs,cjs,json,tsx,css,less,scss,vue,html,md}": [
+		'cspell lint "(packages|apps)/**/*.{js,ts,mjs,cjs,json,tsx,css,less,scss,vue,html,md}"'
+	],
 	"*.{js,ts,vue,md}": ["prettier --write", "eslint"]
 };
